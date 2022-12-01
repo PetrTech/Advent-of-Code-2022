e6d719307afffb29c7ebba6c5f9fa93888e7d4e5
PetrTech <55279432+PetrTech@users.noreply.github.com>
2022-12-01 15:32:27 +0100

    Day 1

---
+++ 1/Part1/1_1/1_1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp = File.ReadAllText("input.txt");
+
+            // sum all numbers that are split in sections and return the largest one
+            string[] numberSections = inp.Split("\r\n\r\n"); // get all sections
+            int highestSum = 0;
+
+            foreach (string section in numberSections)
+            {
+                int sum = 0;
+                string[] numbers = section.Split("\r\n"); // get all numbers into an array
+
+                foreach(string number in numbers)
+                {
+                    sum += int.Parse(number);
+                }
+
+                if(sum > highestSum)
+                {
+                    highestSum = sum;
+                }
+            }
+
+            Console.WriteLine(highestSum);
+        }
+    }
+}
---
+++ 1/Part2/1_2/1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Do the same thing as part 1, but get the top 3
+
+namespace _1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp = File.ReadAllText("input.txt");
+
+            string[] numberSections = inp.Split("\r\n\r\n"); // get all sections
+            List<int> highestSums = new();
+
+            foreach (string section in numberSections)
+            {
+                int sum = 0;
+                string[] numbers = section.Split("\r\n"); // get all numbers into an array
+
+                foreach (string number in numbers)
+                {
+                    sum += int.Parse(number);
+                }
+
+                highestSums.Add(sum);
+            }
+
+            highestSums.Sort();
+
+            Console.WriteLine(highestSums[highestSums.Count - 1] + highestSums[highestSums.Count - 2] + highestSums[highestSums.Count - 3]);
+        }
+    }
+}
